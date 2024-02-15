c66d9a99bf80936ee4a273bf1916db95cc41b89a
Rahad-Ullah <myemail.abc20@gmail.com>
2024-02-15 15:51:10 +0600

    Parent component has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Parent from './components/Parent/Parent'
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Parent/>
     </>
   )
 }
---
+++ src/components/Parent/Parent.jsx
@@ -0,0 +1,12 @@
+
+
+const Parent = () => {
+    return (
+        <div className="border-2 border-yellow-500 rounded-md h-96">
+            <p className="text-left m-4 mx-6 font-medium">Parent</p>
+
+        </div>
+    );
+};
+
+export default Parent;

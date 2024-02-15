db4008d8c2ce01405849bb26ad727666f91cc0db
Rahad-Ullah <myemail.abc20@gmail.com>
2024-02-15 16:18:22 +0600

    Child-2 has been added

--- src/components/Child_2/Child_2.jsx
+++ src/components/Child_2/Child_2.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-const Child_1 = () => {
+const Child_2 = () => {
     const [counter, setCounter] = useState(0)
     
     return (
-        <div className="w-1/2 mx-auto border-2 border-green-500 pb-8 mb-8">
-            <p className="m-4 mx-6 font-medium">Child-1</p>
+        <div className="w-1/2 mx-auto border-2 border-green-500 pb-8">
+            <p className="m-4 mx-6 font-medium">Child-2</p>
             <div className="flex justify-center items-center gap-4 pt-6">
                 <button onClick={() => counter > 0 && setCounter(prev => prev - 1)} className="bg-red-500 px-4 py-2 rounded-lg text-white font-medium hover:bg-red-700">Decrement</button>
                 <h3 className="text-4xl">{counter}</h3>
@@ -15,4 +15,4 @@ const Child_1 = () => {
     );
 };
 
-export default Child_1;
+export default Child_2;
--- src/components/Parent/Parent.jsx
+++ src/components/Parent/Parent.jsx
@@ -1,12 +1,14 @@
 import Child_1 from "../Child_1/Child_1";
+import Child_2 from "../Child_2/Child_2";
 
 
 const Parent = () => {
     return (
-        <div className="border-2 border-yellow-500 rounded-md h-96">
-            <p className="text-left m-4 mx-6 font-medium">Parent</p>
+        <div className="border-2 border-yellow-500 rounded-md p-6">
+            <p className="text-left mx-2 font-medium">Parent</p>
 
             <Child_1/>
+            <Child_2/>
         </div>
     );
 };
